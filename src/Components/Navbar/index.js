c28d1697f31bcbedf434f6853cb4741fa0a469aa
mazahirharoon <mazahir.haroon@bigbinary.com>
2022-08-15 04:23:37 +0530

    Created Navbar Component

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,19 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Search from "./Search";
 
 function Navbar() {
-  //   const [movies, setMovies] = useState([]);
-  //   useEffect(() => {
-  //     fetchData();
-  //   }, []);
-
-  //   const fetchData = async () => {
-  //     const response = await axios.get(fetchUrl);
-  //     setMovies(response.data.results);
-  //   };
   return (
-    <div className="navbar">
-      <h1> Navbar </h1>
-    </div>
+    <nav className="flex flex-row bg-gray-300">
+      <div className="text-2xl text-black-700 font-black text-center px-2 py-2 m-2 ml-5">
+        Legal Wiki
+      </div>
+      <Search />
+      <div className="text-sm text-gray-700 text-center px-0.5 py-3 m-2 font-extrabold ml-auto">
+        Username
+      </div>
+      <div className="text-sm text-gray-700 text-center px-0.5 font-extrabold py-3 m-2">
+        Help
+      </div>
+      <div className="text-sm text-gray-700 text-center px-0.5 py-3 m-2">
+        Choose Language
+      </div>
+    </nav>
   );
 }
 
