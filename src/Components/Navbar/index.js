7053233aafce4394a27aced86d1f4d57594dfee5
mazahirharoon <mazahir.haroon@bigbinary.com>
2022-08-15 04:59:16 +0530

    Improve Navbar Semantics
    Made: website name a heading and the navbar elements an unordered list.

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,19 +4,21 @@ import Search from "./Search";
 function Navbar() {
   return (
     <nav className="flex flex-row bg-gray-300">
-      <div className="text-2xl text-black-700 font-black text-center px-2 py-2 m-2 ml-5">
+      <h1 className="text-2xl text-black-700 font-black text-center px-2 py-2 m-2 ml-5">
         Legal Wiki
-      </div>
+      </h1>
       <Search />
-      <div className="text-sm text-gray-700 text-center px-0.5 py-3 m-2 font-extrabold ml-auto">
-        Username
-      </div>
-      <div className="text-sm text-gray-700 text-center px-0.5 font-extrabold py-3 m-2">
-        Help
-      </div>
-      <div className="text-sm text-gray-700 text-center px-0.5 py-3 m-2">
-        Choose Language
-      </div>
+      <ul className="flex flex-row bg-gray-300 ml-auto">
+        <li className="text-sm text-gray-700 text-center px-0.5 py-3 m-2 font-extrabold">
+          <a href="">Username</a>
+        </li>
+        <li className="text-sm text-gray-700 text-center px-0.5 py-3 m-2 font-extrabold">
+          <a href="">Help</a>
+        </li>
+        <li className="text-sm text-gray-700 text-center px-0.5 py-3 m-2">
+          <a href="">Choose Language</a>
+        </li>
+      </ul>
     </nav>
   );
 }
