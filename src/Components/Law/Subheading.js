fa8cd9c187d7d0f8cf9749064db1e513ffe97ec4
mazahirharoon <mazahir.haroon@bigbinary.com>
2022-08-15 05:09:16 +0530

    Created SubHeading Component

--- src/Components/Law/Subheading.js
+++ src/Components/Law/Subheading.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function SubHeading() {
-  //   const [movies, setMovies] = useState([]);
-  //   useEffect(() => {
-  //     fetchData();
-  //   }, []);
-
-  //   const fetchData = async () => {
-  //     const response = await axios.get(fetchUrl);
-  //     setMovies(response.data.results);
-  //   };
   return (
-    <div className="sub-heading">
-      <h1> Sub Heading </h1>
+    <div className="flex justify-between bg-gray-400 text-xs font-bold p-5 px-20">
+      <div>
+        <p>Location</p>
+        <p className="font-normal inline-block">Kerala</p>
+        <button class="bg-white inline-block hover:bg-gray-100 text-gray-800 font-semibold mx-2 px-1 border border-gray-400 rounded shadow">
+          Change
+        </button>
+      </div>
+      <div>
+        <p>Fine</p>
+        <p className="font-normal">
+          <span>&#8377;</span>500
+        </p>
+      </div>
+      <div>
+        <p>Effective from</p>
+        <p className="font-normal">28th October 2020</p>
+      </div>
     </div>
   );
 }
