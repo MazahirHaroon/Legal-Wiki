335d5c7a4a0164fecb2a684bb8148067e438fd8c
mazahirharoon <mazahir.haroon@bigbinary.com>
2022-08-16 15:58:07 +0530

    Removed unused code and files.

--- src/Components/Law/Heading.js
+++ src/Components/Law/Heading.js
@@ -1,15 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 function Heading() {
-  //   const [movies, setMovies] = useState([]);
-  //   useEffect(() => {
-  //     fetchData();
-  //   }, []);
-
-  //   const fetchData = async () => {
-  //     const response = await axios.get(fetchUrl);
-  //     setMovies(response.data.results);
-  //   };
   return (
     <div className="text-center bg-gray-900 text-white p-5 pb-10">
       <h1 className="text-3xl font-bold p-2">Driving without Helmet</h1>
